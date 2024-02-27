3b2efb23324e842a22022cf55f1718cf78d821b1
James Anderson <james.x.anderson@mnscorp.net>
2024-02-27 22:42:34 +0000

    feat: get asset entries

--- assets.go
+++ assets.go
@@ -2,7 +2,9 @@ package gontentful
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/url"
 )
 
 const (
@@ -37,8 +39,24 @@ func (s *AssetsService) Publish(id string, version string) ([]byte, error) {
 	return s.client.put(path, nil)
 }
 
-
 func (s *AssetsService) GetSingle(id string) ([]byte, error) {
 	path := fmt.Sprintf(pathAssetsEntry, s.client.Options.SpaceID, s.client.Options.EnvironmentID, id)
 	return s.client.getCMA(path, nil)
+}
+
+func (s *AssetsService) GetEntries(query url.Values) (*Entries, error) {
+	path := fmt.Sprintf(pathAssets, s.client.Options.SpaceID, s.client.Options.EnvironmentID)
+	data, err := s.client.getCMA(path, query)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(string(data))
+
+	res := &Entries{}
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
